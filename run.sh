bafbc62e375b761db9ed666e06dec000d3b6e969
Daohang Shi <dshi@tux-70.cae.wisc.edu>
2015-05-08 16:12:17 -0500

    add for loop in run.sh

--- run.sh
+++ run.sh
@@ -12,21 +12,25 @@
 #  canneal
 #  dedup
 #  streamcluster
-SCRIPT=blackscholes
-build/X86/gem5.fast \
-configs/example/fs.py \
---disk-image=parsec.img \
---kernel=x86_64-vmlinux-2.6.22.9.smp \
--n 8 \
--r 1 \
---ruby \
---caches \
---restore-with-cpu=timing \
---cpu-type=timing \
---num-l2caches=1 \
---l1d_size=32kB \
---l1i_size=32kB \
---l2_size=1MB \
---script=benchmark_scripts/$SCRIPT.rcS
-#--script=configs/boot/hack_back_ckpt.rcS
 
+for benchmark in {bodytrack, facesim, ferret, freqmine, raytrace, swaptions, fluidanimate, x264, canneal, dedup, streamcluster}
+do
+  SCRIPT=$benchmark
+  build/X86/gem5.fast \
+    --stats-file=$benchmark.txt \
+    configs/example/fs.py \
+    --disk-image=parsec.img \
+    --kernel=x86_64-vmlinux-2.6.22.9.smp \
+    -n 8 \
+    -r 1 \
+    --ruby \
+    --caches \
+    --restore-with-cpu=timing \
+    --cpu-type=timing \
+    --num-l2caches=1 \
+    --l1d_size=32kB \
+    --l1i_size=32kB \
+    --l2_size=1MB \
+    --script=benchmark_scripts/$SCRIPT.rcS
+  #--script=configs/boot/hack_back_ckpt.rcS
+done
